f483495e07106d9b605bad05f8dadbd0f6898759
Aslak Knutsen <aslak@redhat.com>
2011-08-14 17:13:17 +0200

    ARQ-545 Deploy and Undeploy should happen within the same DeploymentContext, Deployment object is used as key.

--- plugin/src/main/java/org/jboss/arquillian/maven/Utils.java
+++ plugin/src/main/java/org/jboss/arquillian/maven/Utils.java
@@ -17,17 +17,24 @@
  */
 package org.jboss.arquillian.maven;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.jboss.arquillian.container.spi.Container;
 import org.jboss.arquillian.container.spi.client.container.DeploymentException;
 import org.jboss.arquillian.container.spi.client.container.LifecycleException;
 import org.jboss.arquillian.container.spi.client.deployment.Deployment;
 import org.jboss.arquillian.container.spi.client.deployment.DeploymentDescription;
+import org.jboss.arquillian.container.spi.client.protocol.metadata.ProtocolMetaData;
 import org.jboss.arquillian.container.spi.event.DeployDeployment;
 import org.jboss.arquillian.container.spi.event.SetupContainer;
 import org.jboss.arquillian.container.spi.event.StartContainer;
 import org.jboss.arquillian.container.spi.event.StopContainer;
 import org.jboss.arquillian.container.spi.event.UnDeployDeployment;
+import org.jboss.arquillian.core.api.Instance;
+import org.jboss.arquillian.core.api.annotation.Inject;
 import org.jboss.arquillian.core.spi.Manager;
+import org.jboss.arquillian.core.spi.NonManagedObserver;
 import org.jboss.shrinkwrap.api.Archive;
 
 /**
@@ -38,6 +45,8 @@ import org.jboss.shrinkwrap.api.Archive;
  */
 final class Utils
 {
+   private static Map<Archive<?>, Deployment> contextMap = new HashMap<Archive<?>, Deployment>();
+   
    private Utils() { }
    
    public static void setup(Manager manager, Container container) throws LifecycleException
@@ -58,17 +67,45 @@ final class Utils
    public static void deploy(Manager manager, Container container, Archive<?> deployment) throws DeploymentException
    {
       manager.fire(new DeployDeployment(
-            container, 
-            new Deployment(
-                  new DeploymentDescription("NO-NAME", deployment))));
-      
+            container,
+            getOrCreateDeployment(deployment)),
+            new NonManagedObserver<DeployDeployment>()
+            {
+               @Inject
+               private Instance<ProtocolMetaData> metadataInst;
+
+               @Override
+               public void fired(DeployDeployment event)
+               {
+                  ProtocolMetaData metadata = metadataInst.get();
+                  if(metadata != null)
+                  {
+                     System.out.println(metadata);
+                  }
+               }
+            });
    }
 
    public static void undeploy(Manager manager, Container container, Archive<?> deployment) throws DeploymentException
    {
       manager.fire(new UnDeployDeployment(
             container, 
-            new Deployment(
-                  new DeploymentDescription("NO-NAME", deployment))));
+            getOrCreateDeployment(deployment)));
+   }
+
+   private static Deployment getOrCreateDeployment(Archive<?> archive)
+   {
+      if(contextMap.containsKey(archive))
+      {
+         return contextMap.remove(archive);
+      }
+      else
+      {
+         Deployment deployment = new Deployment(
+               new DeploymentDescription("NO-NAME", archive));
+         contextMap.put(archive, deployment);
+
+         return deployment;
+      }
    }
 }
